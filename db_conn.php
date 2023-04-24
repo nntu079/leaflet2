d70b349cc7322b8fe8976bbf4086852ab153baa8
Tu <nntu079>
2023-04-25 02:43:54 +0700

    a

--- db_conn.php
+++ db_conn.php
@@ -7,7 +7,6 @@ $dbname = 'geo2';
 $user = 'postgres';
 $password = 'password';
 
-// Kết nối database tintuc
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 if (!$conn) {
     echo "Not connected: ";
